66c7ebb8772e9e0a4abcca030e3a2c9ca986c6eb
riski <riskiyantoriskiyanto054@gmail.com>
2023-03-17 14:44:03 +0700

    menambahkan file calculator.cs

--- calculator.cs
+++ calculator.cs
@@ -2,8 +2,9 @@ class Calculator
 {
     static void Main(string[] args)
     {
-		int a = 1';
+		int a = 10;
 		int b = 6;
+		
 		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a+b));
 		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a-b));
 		
